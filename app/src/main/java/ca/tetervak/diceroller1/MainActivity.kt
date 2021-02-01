ad2dc3729f69cc3199c8a93148c6af14182ebee0
Alex Tetervak <tetervak@sheridancollege.ca>
2021-02-01 17:29:33 -0500

    bound viewmodel to the layout

--- app/src/main/java/ca/tetervak/diceroller1/MainActivity.kt
+++ app/src/main/java/ca/tetervak/diceroller1/MainActivity.kt
@@ -2,54 +2,22 @@ package ca.tetervak.diceroller1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.viewModels
 import ca.tetervak.diceroller1.databinding.ActivityMainBinding
-import ca.tetervak.diceroller1.domain.Die
 
 class MainActivity : AppCompatActivity() {
 
-    companion object{
-        const val TAG = "DieRoller"
-        const val CURRENT_DIE_VALUE = "current_die_value"
-    }
-
-    private lateinit var  binding: ActivityMainBinding
-    private val viewModel: MainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        viewModel.dieValue.observe(this){
-            displayDieValue(it)
-        }
-
-        binding.rollButton.setOnClickListener { onRoll() }
-        binding.resetButton.setOnClickListener { onReset() }
+        val viewModel: MainViewModel by viewModels()
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
 
     }
 
-    private fun onRoll() {
-        Log.d(TAG, "onRoll() called")
-        viewModel.rollDie()
-        Toast.makeText(this, getString(R.string.die_rolled), Toast.LENGTH_SHORT).show()
-    }
-
-    private fun onReset() {
-        Log.d(TAG, "onReset() called ")
-        viewModel.resetDie()
-        Toast.makeText(this, getString(R.string.app_reset), Toast.LENGTH_SHORT).show()
-    }
-
-    private fun displayDieValue(dieValue: String) {
-        Log.d(TAG, "displayDieValue() called ")
-        binding.dieValue.text = dieValue
-    }
 
 }
